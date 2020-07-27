cf7dcde428cb64ab4b0eaa5e089c5d8062590783
Johan Norberg <norberg_johan@hotmail.com>
2020-07-27 16:27:18 +0200

    Use --branches instead of --all for git-history

--- fzf_git.sh
+++ fzf_git.sh
@@ -23,7 +23,7 @@ git-branch() {
 
 git-history() {
   is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always --all |
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always --branches |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
